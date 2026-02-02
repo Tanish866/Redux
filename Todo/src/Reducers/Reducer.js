454f11e903051023a7e5c8e3839ecc790d1aa40e
Tanish Singh <tanishsingh866@gmail.com>
2026-02-02 17:25:29 +0000

    Adding Reducer

---
+++ Todo/src/Reducers/Reducer.js
@@ -0,0 +1,22 @@
+import { ADD_TODO, EDIT_TODO, REMOVE_TODO } from "../constants";
+
+function Reducer(todos = [], action){
+    if(action.type == ADD_TODO){
+        return [...todos, {id: action.payload.id, title: action.payload.title}];
+    }
+    if(action.type == EDIT_TODO){
+        return todos.map(todo => {
+            if(action.payload.id == todo.id){
+                todo.title = action.payload.title;
+            }
+            return todo;
+        });
+    }
+    if(action.type == REMOVE_TODO){
+        return todos.filter(todo => {
+            if(action.payload != todo.id);
+        })
+    }
+    return todos;
+}
+export default Reducer;
